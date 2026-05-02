e261ad065df14210090ac04dfd9e06bb3fb0f2d7
gastonledesma328-dot <gastonledesma328@gmail.com>
2026-05-01 23:50:02 -0300

    Add files via upload

---
+++ assets/_tanstack-start-manifest_v-Dv1L-qze.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Gasti/Downloads/clone-from-vision-main/clone-from-vision-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-COALA_Gu.js"] }, "/": { filePath: "C:/Users/Gasti/Downloads/clone-from-vision-main/clone-from-vision-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-5Wwm1R8h.js"] } }, clientEntry: "/assets/index-COALA_Gu.js" });
+export {
+  tsrStartManifest
+};
